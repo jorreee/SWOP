d643fb6664e6bf6b4f487e1b6f44b9d88fea7936
TimVDB <tim.vandenbroecke@student.kuleuven.be>
2015-03-09 22:48:28 +0100

    Continued UseCase3Test

--- src/taskMan/test/UseCase3CreateTaskTest.java
+++ src/taskMan/test/UseCase3CreateTaskTest.java
@@ -15,18 +15,20 @@ public class UseCase3CreateTaskTest {
 	private TaskMan taskMan;
 	private final LocalDateTime startDate = LocalDateTime.of(2015, 2, 9, 8, 0),
 			project1StartDate = startDate,
-			project1DueDate = LocalDateTime.of(2015, 2, 13, 23, 59);
+			project1DueDate = LocalDateTime.of(2015, 2, 13, 23, 59),
+			workDate = LocalDateTime.of(2015, 2, 9, 16, 0),
+			altTaskEndDate = LocalDateTime.of(2015, 2, 9, 10, 0);
 	private final int task11EstDur = 8*60,
 					  newTaskDur = 5*60;
 	private final int task11Dev = 0,
 					  newTaskDev = 10;
 	private final ArrayList<Integer> task11Dependencies = new ArrayList(),
-									 newTaskDependencies = new ArrayList();
+									 newTaskDependencies = new ArrayList<Integer>();
 
 	/**
 	 * DEFAULT TASKMAN TESTER
 	 * - project 1 START 9 feb DUE 13 feb (midnight)
-	 * 		task 1			
+	 * 		GEEN tasks aanwezig
 	 */
 	@Before
 	public final void initialize() {
@@ -34,8 +36,9 @@ public class UseCase3CreateTaskTest {
 
 		taskMan.createProject("Test1", "testing 1", project1StartDate, project1DueDate);
 
-		
-		taskMan.createTask(1, "Design system", task11EstDur, task11Dev, null, task11Dependencies);		// TASK 1
+		taskMan.advanceTimeTo(workDate);
+//		
+//		taskMan.createTask(1, "Design system", task11EstDur, task11Dev, null, task11Dependencies);		// TASK 1
 //		task12Dependencies.add(Integer.valueOf(1));
 //		taskMan.createTask(1, "Implement Native", task12EstDur, task12Dev, null, task12Dependencies);	// TASK 2
 //		task13Dependencies.add(Integer.valueOf(2));
@@ -45,14 +48,13 @@ public class UseCase3CreateTaskTest {
 
 	}
 	
-	//TODO alternative task + dependencies
-	//TODO als ……N alt task voor task X slaagt, vervallen de anderen dan?
+	//TODO als ……N alt task voor task X slaagt, vervallen de andere ALT voor X dan?
 
 	@Test
 	public void SuccesCaseTest() {
 		// Step 1 and 2 are implicit
 		// Step 3
-		taskMan.createTask(1, "A new TASK", newTaskDur, newTaskDev, null, newTaskDependencies);
+		taskMan.createTask(1, "A new TASK", newTaskDur, newTaskDev, null, newTaskDependencies, LocalDateTime());
 		// Step 4
 		assertTrue(taskMan.getTaskDescription(1,1).equals("A new TASK"));
 		assertEquals(taskMan.getTaskEstimatedDuration(1,1),newTaskDur);
@@ -68,9 +70,14 @@ public class UseCase3CreateTaskTest {
 	//TODO Alt testen
 	@Test
 	public void SuccesCaseALTTest() {
+
+		// Er is al een task aanwezig in het project
+		taskMan.createTask(1, "Design system", task11EstDur, task11Dev, null, task11Dependencies);
+		taskMan.updateTaskDetails(1, 1, startDate, altTaskEndDate, "failed");
+		
 		// Step 1 and 2 are implicit
 		// Step 3
-		taskMan.createTask(1, "A new TASK", newTaskDur, newTaskDev, null, newTaskDependencies);
+		taskMan.createTask(1, "A new TASK", newTaskDur, newTaskDev, Integer.valueOf(1), newTaskDependencies);
 		// Step 4
 		assertTrue(taskMan.getTaskDescription(1,1).equals("A new TASK"));
 		assertEquals(taskMan.getTaskEstimatedDuration(1,1),newTaskDur);
@@ -103,7 +110,7 @@ public class UseCase3CreateTaskTest {
 	
 	@Test
 	public void flow3aTest() {
-		// De UI zal geen request doorsturen.
+		// De UI zal geen request doorsturen als de user geen volledig formulier invult.
 		assertTrue(taskMan.getProjectNumberOfTasks(1) == 0); //TODO Moet dit kunnen worden opgevraagd?
 	}
 	
