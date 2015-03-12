09896b27ffe704ec84e8e75ade16ee0b2b02d597
Vincent Van Gestel <vincent.vgestel@telenet.be>
2015-03-12 11:45:26 +0100

    Ongeveer de helft van de UI getest op UI bugs

--- src/userInterface/InputParser.java
+++ src/userInterface/InputParser.java
@@ -7,37 +7,42 @@ import userInterface.requests.CreateProjectRequest;
 import userInterface.requests.CreateTaskRequest;
 import userInterface.requests.ExitRequest;
 import userInterface.requests.HelpRequest;
+import userInterface.requests.InvalidRequest;
 import userInterface.requests.Request;
 import userInterface.requests.ShowProjectsRequest;
 import userInterface.requests.UpdateTaskStatusRequest;
 
 public class InputParser {
-	
+
 	private IFacade facade;
 	private BufferedReader inputReader;
-	
+
 	public InputParser(IFacade facade, BufferedReader inputReader) {
 		this.facade = facade;
 		this.inputReader = inputReader;
 	}
-	
+
 	public Request parse(String readLine) {
 		String[] input = readLine.split(" ");
-		
+
 		// Switch on first word
-		switch(input[0].toLowerCase()) {
-		case "h"		: return new HelpRequest(facade, inputReader);
-		case "exit"		: return new ExitRequest(facade, inputReader);
-		case "advance"	: return new AdvanceTimeRequest(facade, inputReader);
-		case "update"	: if(input[1].toLowerCase().equals("task"))
-							return new UpdateTaskStatusRequest(facade, inputReader);
-		case "create"	: if(input[1].toLowerCase().equals("task"))
-							return new CreateTaskRequest(facade, inputReader);
-						  if(input[1].toLowerCase().equals("project"))
-							return new CreateProjectRequest(facade, inputReader);
-		case "show"		: return new ShowProjectsRequest(facade, inputReader);
+		try {
+			switch(input[0].toLowerCase()) {
+			case "h"		: return new HelpRequest(facade, inputReader);
+			case "exit"		: return new ExitRequest(facade, inputReader);
+			case "advance"	: return new AdvanceTimeRequest(facade, inputReader);
+			case "update"	: if(input[1].toLowerCase().equals("task"))
+				return new UpdateTaskStatusRequest(facade, inputReader);
+			case "create"	: if(input[1].toLowerCase().equals("task"))
+				return new CreateTaskRequest(facade, inputReader);
+			if(input[1].toLowerCase().equals("project"))
+				return new CreateProjectRequest(facade, inputReader);
+			case "show"		: return new ShowProjectsRequest(facade, inputReader);
+			default	: return new InvalidRequest(facade, inputReader);
+			}
+		}catch(Exception e) {
+			return new InvalidRequest(facade, inputReader);
 		}
-		return null;
 	}
-	
 }
+
--- src/userInterface/requests/AdvanceTimeRequest.java
+++ src/userInterface/requests/AdvanceTimeRequest.java
@@ -15,7 +15,7 @@ public class AdvanceTimeRequest extends Request {
 	public String execute() {
 		LocalDateTime currentTime = facade.getCurrentTime();
 		//Allow user to choose new timestamp
-		System.out.println("The current time is " + currentTime.toString() + ", advance time to? (type quit to exit)");
+		System.out.println("The current time is " + currentTime.toString() + ", advance time to? (Format: Y M D H M, type quit to exit)");
 		while(true) {
 			try {
 				String input = inputReader.readLine();
@@ -25,14 +25,15 @@ public class AdvanceTimeRequest extends Request {
 					return "Time unaltered";
 
 				// Parse timestamp
-				LocalDateTime time = LocalDateTime.parse(input);
+				String[] inputParts = input.split(" ");
+				LocalDateTime time = LocalDateTime.of(Integer.parseInt(inputParts[0]), Integer.parseInt(inputParts[1]), Integer.parseInt(inputParts[2]), Integer.parseInt(inputParts[3]), Integer.parseInt(inputParts[4]));
 				
 				// Advance time
 				boolean success = facade.advanceTimeTo(time);
 				
 				// Invalid timestamp
 				if(!success) {
-					System.out.println("Invalid time");
+					System.out.println("Invalid time, try again");
 				} else {// Correct timestamp
 					return "Time advanced to " + time.toString();
 				}
