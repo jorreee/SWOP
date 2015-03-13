98908169c95c99af82b6f5665bfbe05ce32a6cef
TimVDB <tim.vandenbroecke@student.kuleuven.be>
2015-03-13 14:39:54 +0100

    Added new testFiles

---
+++ src/taskMan/test/TaskManTest.java
@@ -0,0 +1,14 @@
+package taskMan.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskManTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/taskMan/test/TaskTest.java
@@ -0,0 +1,23 @@
+package taskMan.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import taskMan.Task;
+import taskMan.util.TimeSpan;
+
+public class TaskTest {
+	
+	private final TimeSpan extraTimeGood = new TimeSpan(50);
+
+	@Test()
+	public void contructorSuccesTest() {
+		Task goodTask = new Task(1, "Descr", 5*60, 10, extraTimeGood);
+		assertEquals(1,goodTask.getTaskID());
+		assertEquals("Descr",goodTask.getDescription());
+		assertEquals(new TimeSpan(5*60),goodTask.getEstimatedDuration());
+		assertEquals(10,goodTask.getAcceptableDeviation());
+		assertEquals(extraTimeGood,goodTask.getTimeSpan());
+	}
+
+}
