3ff4c0dad75f52c05f20119a6e6b5c99a648cd78
Vincent Van Gestel <vincent.vgestel@telenet.be>
2015-03-30 16:53:13 +0200

    Nog eens committen

--- src/taskMan/resource/ResourcePool.java
+++ src/taskMan/resource/ResourcePool.java
@@ -1,9 +1,36 @@
 package taskMan.resource;
 
-public class ResourcePool {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ResourcePool {
+	
+	// The prototype
+	private final ResourcePrototype resProt;
+	
+	// A list of concreteResourceInstances
+	private List<ConcreteResource> concreteResList;
+	
 	public ResourcePool(ResourcePrototype resPrototype) {
-		// TODO Auto-generated constructor stub
+		this.resProt = resPrototype;
+		this.concreteResList = new ArrayList<>();
+	}
+
+	public ResourcePrototype getPrototype() {
+		return resProt;
+	}
+
+	public boolean createResourceInstance(String resName) {
+		ConcreteResource conRes = resProt.clone();
+		conRes.setName(resName);
+		return concreteResList.add(conRes);
+	}
+
+	public Resource getConcreteResourceByIndex(int concreteResourceIndex) {
+		if(concreteResourceIndex < 0 || concreteResourceIndex > concreteResList.size()) {
+			return null;
+		}
+		return concreteResList.get(concreteResourceIndex);
 	}
 
 }
