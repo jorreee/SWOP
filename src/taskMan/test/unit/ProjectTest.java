c1a3d79fa48bedb076c12f39058771fae34078cc
Eli Vangrieken <elivangr@gmail.com>
2015-03-30 16:44:50 +0200

    added some top project test

--- src/taskMan/test/unit/ProjectTest.java
+++ src/taskMan/test/unit/ProjectTest.java
@@ -6,9 +6,18 @@ import org.junit.*;
 
 import static org.junit.Assert.*; 
 import taskMan.Project;
+import taskMan.state.*;
 
 public class ProjectTest {
 //TODO
+	
+	private Project defaultProject;
+	
+	@Before
+	public void initialize(){
+		defaultProject = new Project(1,"defaultProject","default",
+				LocalDateTime.of(2015, 2, 11, 16, 0),LocalDateTime.of(2015, 2, 17, 16, 0));
+	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void ConstrFailID(){
@@ -44,6 +53,51 @@ public class ProjectTest {
 	public void ConstrFailDueBeforeCreation(){
 		new Project(1,"fail","fail",LocalDateTime.of(2015, 2, 12, 16, 0),
 				LocalDateTime.of(2015, 2, 11, 16, 0));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void ConstrFailDueEqualsCreation(){
+		new Project(1,"fail","fail",LocalDateTime.of(2015, 2, 12, 16, 0),
+				LocalDateTime.of(2015, 2, 12, 16, 0));
+	}
+	
+	@Test
+	public void getIDTest(){
+		assertEquals(1, defaultProject.getProjectID());
+	}
+	
+	@Test
+	public void getNameTest(){
+		assertEquals("defaultproject", defaultProject.getProjectName().toLowerCase());
+	}
+	
+	@Test
+	public void getDescriptionTest(){
+		assertEquals("default", defaultProject.getProjectDescription().toLowerCase());
+	}
+	
+	@Test
+	public void getCreationTimeTest(){
+		assertEquals(LocalDateTime.of(2015, 2, 11, 16, 0), 
+				defaultProject.getProjectCreationTime());
+	}
+	
+	@Test
+	public void getDueTimeTest(){
+		assertEquals(LocalDateTime.of(2015, 2, 17, 16, 0), 
+				defaultProject.getProjectDueTime());
+	}
+	
+	@Test
+	public void getStateTest(){
+		assertEquals("ongoing", defaultProject.getProjectStatus().toLowerCase());
+	}
+	
+	@Test
+	public void setFinishedTest(){
+		assertFalse(defaultProject.isFinished());
+		defaultProject.setProjectStatus(new FinishedProject(defaultProject));
+		assertTrue(defaultProject.isFinished());
 	}
 	
 }
