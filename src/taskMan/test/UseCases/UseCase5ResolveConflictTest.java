e84592e560766efac21e8b16a567e832d5fb5321
Eli Vangrieken <elivangr@gmail.com>
2015-05-01 12:21:38 +0200

    working on 5

--- src/taskMan/test/UseCases/UseCase5ResolveConflictTest.java
+++ src/taskMan/test/UseCases/UseCase5ResolveConflictTest.java
@@ -131,7 +131,6 @@ public class UseCase5ResolveConflictTest {
 		project00 = conflicts.keySet().iterator().next();
 		assertEquals(1,conflicts.get(project00).size());
 		assertEquals(task00, conflicts.get(project00).get(0));
-		//TODO Error: planning slaagt niet
 		assertTrue(taskManager.planTask(project00, task00, task02StartDateGood, task00ConcreteResources, devList1));
 		
 		conflicts = taskManager.findConflictingPlannings(task02);
@@ -212,8 +211,39 @@ public class UseCase5ResolveConflictTest {
 		
 		assertTrue(taskManager.planTask(project00, task00, task00StartDateGood, task00ConcreteResources, devList1));
 		
-		assertTrue(taskManager.createProject("project2", "test", startDate));
+		assertTrue(taskManager.createProject("project2", "test", project0DueDate));
 		ProjectView project01 = taskManager.getProjects().get(1);
-
+		assertTrue(taskManager.createTask(project01, "task10", 60, 10, new ArrayList<TaskView>(), task03Res, null));
+		TaskView task10 = project01.getTasks().get(0);
+		
+		task03ConcreteResources.add(taskManager.getResourcePrototypes().get(2));
+		
+		assertTrue(taskManager.planTask(project01, task10, task00StartDateGood, task03ConcreteResources, devList2));
+		
+		HashMap<ResourceView, Integer >reqRes = new HashMap<ResourceView,Integer>();
+		reqRes.put(taskManager.getResourcePrototypes().get(2), 1);
+		assertTrue(taskManager.createTask(project00, "test", 60, 5, new ArrayList<TaskView>(), reqRes, null));
+		TaskView test = project00.getTasks().get(4);
+		ArrayList<ResourceView> concRes = new ArrayList<ResourceView>();
+		concRes.add(taskManager.getResourcePrototypes().get(2));
+		assertTrue(taskManager.planRawTask(project00, test, task00StartDateGood, concRes, devList1));	
+		
+		Map<ProjectView, List<TaskView>> conflicts = taskManager.findConflictingPlannings(test);
+		assertEquals(2,conflicts.keySet().size());
+		
+		ProjectView[] con = new ProjectView[2];
+		con = conflicts.keySet().toArray(con);
+		project00 = con[0];
+		project01 = con[1];
+		assertEquals(1,conflicts.get(project00).size());
+		assertEquals(1,conflicts.get(project01).size());
+		assertTrue(conflicts.get(project00).contains(task00));
+		assertTrue(conflicts.get(project01).contains(task10));
+		
+		assertTrue(taskManager.planTask(project00, task00, task02StartDateGood, task00ConcreteResources, devList1));
+		assertTrue(taskManager.planTask(project01, task10, task02StartDateGood, task03ConcreteResources, devList2));
+		
+		conflicts = taskManager.findConflictingPlannings(test);
+		assertTrue(conflicts.isEmpty());
 	}
 }
