ddaa159a370813717be601086e170dc5c1f453ca
TimVDB <tim.vandenbroecke@student.kuleuven.be>
2015-05-15 01:05:13 +0200

    EJ

---
+++ src/exceptions/IllegalWhoopsieException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class IllegalWhoopsieException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public IllegalWhoopsieException(String s) {
+		super(s);
+	}
+
+}
