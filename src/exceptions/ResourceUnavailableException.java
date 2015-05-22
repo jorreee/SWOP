d4fd7b136386120f1d3da950a47d4e337a8dd0b9
Vincent Van Gestel <vincent.vgestel@telenet.be>
2015-05-22 11:58:08 +0200

    whoops deze was nog niet opgeslagen

--- src/exceptions/ResourceUnavailableException.java
+++ src/exceptions/ResourceUnavailableException.java
@@ -2,6 +2,12 @@ package exceptions;
 
 import java.rmi.UnexpectedException;
 
+/**
+ * An exception to be used when a resource is not available
+ * 
+ * @author Tim Van den Broecke, Joran Van de Woestijne, Vincent Van Gestel and
+ *         Eli Vangrieken
+ */
 public class ResourceUnavailableException extends UnexpectedException {
 
 	private static final long serialVersionUID = 1L;
